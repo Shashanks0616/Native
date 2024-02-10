8731962898a1c2b9d5f9a5653ec55f7de77865df
Shashanks0616 <shashanksingh0616@gmail.com>
2024-02-10 16:56:52 +0530

    Details screen on exercise

--- Workouts/App.js
+++ Workouts/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import exercises from '../../assets/data/exercises.json';
-import ExerciseListItem from '../components/ExerciseListItem';
+import exercises from './assets/data/exercises.json';
+import ExerciseListItem from './src/components/ExerciseListItem';
 
 export default function App() {
   return (
@@ -21,7 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
     padding: 10,
+    paddingTop: 70,
   },
 });
